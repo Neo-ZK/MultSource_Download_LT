32eaffdeeb6ce6eed242c56ed22f7959c5385ef3
root <root@localhost.localdomain>
2019-02-20 06:51:16 -0800

    完成了LT码的生成矩阵和内容矩阵类的基本编写

--- LT/matrix.cpp
+++ LT/matrix.cpp
@@ -1,15 +1,56 @@
 #include "matrix.h"
 
 
+//generate vector and Matrix
+unsigned short int& Vector_g::operator[](int colIndex){
+    assert(colIndex < col_);
+    return vG_[colIndex];
+}
+
+std::shared_ptr<Vector_g>& Matrix_g::operator[](int rowIndex){
+    assert(rowIndex < row_);
+    return mG_[rowIndex];
+}
+
+
+//content vector and Matrix
 int Vector_content::setValue(char* src,int len){
     if(len != col_){
         return 0;
     }else{
-        memcpy(&(*content_),src,len);
+        memcpy(&(*vContent_),src,len);
         return 1;
     }
 }
 
 int Matrix_content::setVectorContent(int rowIndex,char* src,int len){
-    return content_[rowIndex]->setValue(src,len);
+    if(rowIndex < 0 || rowIndex > row_ || col_ != len){
+        return 0;
+    }else{
+        return mContent_[rowIndex]->setValue(src,len);
+    }
+}
+
+int Matrix_content::setVectorContent(int rowIndex,std::shared_ptr<Vector_content> v){
+    if(rowIndex < 0 || rowIndex > row_ || col_ != v->col_){
+        return 0;
+    }else{
+        mContent_[rowIndex] = v;
+        return 1;
+    }
+}
+
+int Matrix_content::insertVectorToContent(std::shared_ptr<Vector_content> v){
+    if(insertIndex_ >= row_){
+        row_ = row_*2;
+        mContent_.resize(row_);
+    }
+    mContent_[insertIndex_] = v;
+    insertIndex_++;
+    return 1;
+}
+
+std::shared_ptr<Vector_content>& Matrix_content::operator[](int rowIndex){
+    assert(rowIndex < row_);
+    return mContent_[rowIndex];
 }
--- LT/matrix.h
+++ LT/matrix.h
@@ -4,15 +4,70 @@
 #include<memory>
 #include<vector>
 #include<string.h>
+#include<assert.h>
+
+//statement of matrix of generate vector
+class Vector_g{
+public:
+    Vector_g(int col):col_(col){
+        printf("create Vector_g,\n");
+        vG_ = new unsigned short int[col];
+    };
+    ~Vector_g(){
+        printf("delete Vector_g,\n");
+        delete [] vG_;
+    };
+
+public:
+    int col_;
+    unsigned short int* vG_;
+
+public:
+    unsigned short int& operator[](int colIndex);
+};
+
+class Matrix_g{
+public:
+    Matrix_g(int row,int col):row_(row),col_(col),mG_(row){
+        printf("create Matrix_g,\n");
+        for(int i = 0;i < row;i++){
+            mG_[i] = std::make_shared<Vector_g>(col);
+        }
+    };
+    ~Matrix_g(){
+        printf("delete Matrix_g,\n");
+        mG_.erase(mG_.begin(),mG_.end());
+    };
+public:
+    int row_;
+    int col_;
+    std::vector<std::shared_ptr<Vector_g>> mG_;
+
+public:
+    std::shared_ptr<Vector_g>& operator[](int rowIndex);
+
+};
 
 
+
+//statement of matrix of content
 class Vector_content{
 
 public:
-    Vector_content(int col):col_(col),content_(new unsigned char[col]){printf("create Vector,\n");};
-    ~Vector_content(){printf("delete Vector,\n");};
+    Vector_content(int col):col_(col){
+        printf("create Vector_content,\n");
+        vContent_ = new unsigned char[col];
+    };
+    ~Vector_content(){
+        printf("delete Vector_content,\n");
+        delete [] vContent_;
+    };
+
+public:
     int col_;
-    std::shared_ptr<unsigned char> content_;
+    unsigned char* vContent_;
+
+public:
     int setValue(char* src,int len);
 
 };
@@ -20,22 +75,29 @@ public:
 class Matrix_content{
 
 public:
-    Matrix_content(int row,int col):row_(row),col_(col),content_(row){
-        printf("create Matrix,\n");
+    Matrix_content(int row,int col):row_(row),col_(col),insertIndex_(0),mContent_(row){
+        printf("create Matrix_content,\n");
+
         for(int i = 0;i < row;i++){
-            content_[i] = std::make_shared<Vector_content>(col);
+            mContent_[i] = std::make_shared<Vector_content>(col);
         }
     };
     ~Matrix_content(){
-        printf("delete Matrix");
-        content_.erase(content_.begin(),content_.end());
+        printf("delete Matrix_content");
+        mContent_.erase(mContent_.begin(),mContent_.end());
     };
+//variable member
+public:
     int row_;
     int col_;
-    std::vector<std::shared_ptr<Vector_content>> content_;
-
+    int insertIndex_;
+    std::vector<std::shared_ptr<Vector_content>> mContent_;
+//fucntion
 public:
-    int setVectorContent(int row_index,char* src,int len);
+    int setVectorContent(int rowIndex,char* src,int len);
+    int setVectorContent(int rowIndex,std::shared_ptr<Vector_content> v);
+    int insertVectorToContent(std::shared_ptr<Vector_content> v);
+    std::shared_ptr<Vector_content>& operator[](int rowIndex);
 };
 
 #endif // MATRIX_H_INCLUDED
